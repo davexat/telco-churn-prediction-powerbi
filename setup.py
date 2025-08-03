3a8b9a0b60b866d5f1d8e167d6460b1e7cda7b7c
davexat <yisuscristh20@gmail.com>
2025-08-02 23:43:53 -0500

    setup: project module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telco-churn-prediction-powerbi',
+    version='0.0.1',
+    description='Análisis y predicción de churn en clientes de Telco con visualización en Power BI',
+    author='David Sandoval',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=[],
+    include_package_data=True,
+)
